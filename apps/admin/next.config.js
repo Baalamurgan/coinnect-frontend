b430cf947b1003eecde36269001fe2e962f4ce42
Baalamurgan <baalamurgan2307@gmail.com>
2025-01-10 19:43:07 +0530

    feat: admin switch from mui -> shadcn template

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.slingacademy.com',
+        port: ''
+      }
+    ]
+  },
+  transpilePackages: ['geist']
 };
 
 module.exports = nextConfig;
